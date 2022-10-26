973ef396071bdff2b72b77b0653891a127f393e2
Divyansh <dbokadia@gmail.com>
2022-10-26 17:12:33 -0500

    Procfile changes for heroku

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,7 +15,13 @@ more_movies = [
   {:title => 'Nomadland', :rating => 'R',
    :release_date => '19-Feb-2021'},
   {:title => 'CODA', :rating => 'PG-13',
-   :release_date => '13-Aug-2021'}
+   :release_date => '13-Aug-2021'},
+  {:title => 'Shutter Island', :rating => 'R',
+   :release_date => '19-Feb-2010'},
+  {:title => 'Inception', :rating => 'PG-13',
+   :release_date => '13-Jul-2010'},
+  {:title => 'Terminator', :rating => 'R',
+   :release_date => '26-Oct-1986'}
 ]
 
 more_movies.each do |movie|
